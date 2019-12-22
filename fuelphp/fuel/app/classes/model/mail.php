8ca9fe231c4c505cfc15f1ed7690838a99fd07cd
小野裕輔 <y-ono@nswt.co.jp>
2019-12-22 13:39:28 +0900

    Mailモデルで設定ファイルの内容を使うようにした。

--- fuelphp/fuel/app/classes/model/mail.php
+++ fuelphp/fuel/app/classes/model/mail.php
@@ -20,11 +20,13 @@ class Model_Mail extends Model
     // メールの作成
     protected function build_mail($post)
     {
+        Config::load('contact_form', true);
+        
         $data['from']      = $post['email'];
         $data['from_name'] = $post['name'];
-        $data['to']        = 'info@example.jp';
-        $data['to_name']   = '管理者';
-        $data['subject']   = 'コンタクトフォーム';
+        $data['to']        = Config::get('contact_form.admin_email');
+        $data['to_name']   = Config::get('contact_form.admin_name');
+        $data['subject']   = Config::get('contact_form.subject');
 
         $ip    = Input::ip();
         $agent = input::user_agent();
